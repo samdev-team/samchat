f12b923cb17b7e644db9651d0700dff914e36528
ALTERA-LLC <62931899+ALTERA-LLC@users.noreply.github.com>
2020-11-08 09:27:37 +0800

    Update server.py

--- server/server.py
+++ server/server.py
@@ -36,9 +36,8 @@ class Main:
         users = []
         for i in self.clients:
             users.append(i['name'])
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(0.5)
         s.send(str(users).encode('utf-8'))
-        await asyncio.sleep(0.1)
         Thread(target=lambda:asyncio.run(self.client_thread()), daemon=True).start()
         await self.send(username + " has joined the chat")
 
